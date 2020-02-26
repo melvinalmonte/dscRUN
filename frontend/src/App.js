3956a578bddaf779af4d315d14b0805806edb86f
Melvin Almonte <melvinalmonte@gmail.com>
2020-02-25 22:44:41 -0500

    added a landing page and reworked routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Route, Router } from "react-router-dom";
 import { firebaseAuth } from "./firebase/config";
 import Layout from "./components/layout";
 import history from "./history";
-import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 import PublicRoute from "./routes/PublicRoute";
 import { SignUp } from "./pages/SignUp";
 import PrivateRoute from "./routes/PrivateRoute";
@@ -11,6 +11,7 @@ import { Clubs } from "./pages/Clubs";
 import { Groups } from "./pages/Groups";
 import { Availability } from "./pages/Availability";
 import { Redirect } from "./pages/Redirect";
+import {LandingPage} from "./pages/LandingPage";
 
 function App() {
   const [auth, setAuth] = useState(false);
@@ -41,7 +42,8 @@ function App() {
 
   return (
     <Router history={history}>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={LandingPage} />
+      <PublicRoute auth={auth} path="/login" component={Login} />
       <PublicRoute auth={auth} path="/redirect" component={Redirect} />
       <PublicRoute auth={auth} path="/signup" component={SignUp} />
       <PrivateRoute auth={auth} path="/clubs" component={Clubs} />
