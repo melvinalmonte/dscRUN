2d5c1bc004aad891903b86f93bd2d7f86d1dc356
Melvin Almonte <melvinalmonte@gmail.com>
2020-02-27 19:37:33 -0500

    can write to cloud firestore collections as well as retrieve group name from collections

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -3,11 +3,12 @@ import firebase from "firebase";
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID
 };
 
 firebase.initializeApp(config);
 
 export const ref = firebase.database().ref();
 export const firebaseAuth = firebase.auth;
-export const firebaseDB = firebase.database();
+export const db = firebase.firestore();
